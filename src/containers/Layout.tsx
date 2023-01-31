45bf1b5c823839ec320c7df64d96dd405e0d0d47
tojonir <tojonirinaa355@gmail.com>
2023-01-31 21:35:11 +0300

    seting up layout

---
+++ src/containers/Layout.tsx
@@ -0,0 +1,13 @@
+import { PageProps } from "gatsby";
+import React, { FC } from "react";
+import "../styles/index.css";
+
+const Layout: FC<PageProps> = () => {
+  return (
+    <main>
+      <div>tojo</div>
+    </main>
+  );
+};
+
+export default Layout;
