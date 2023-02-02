e66ba64055ddee990d976b11cb25100ef0576ae1
tojonir <tojonirinaa355@gmail.com>
2023-02-02 15:37:10 +0300

    clean

---
+++ src/pages/project.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FC } from "react";
+import Layout from "../containers/Layout";
+
+const Project: FC = () => {
+  return (
+    <Layout>
+      <div>project</div>
+    </Layout>
+  );
+};
+
+export default Project;
