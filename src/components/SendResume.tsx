d8541948d6ffdf040e67d9bc36f8bb400c32a5ef
tojonir <tojonirinaa355@gmail.com>
2023-02-08 18:44:52 +0300

    footer responsive

--- src/components/SendResume.tsx
+++ src/components/SendResume.tsx
@@ -9,7 +9,7 @@ interface SendResumeProps {
 
 const SendResume: FC<SendResumeProps> = ({ placeholder = "Email" }) => {
   return (
-    <div className="flex items-center rounded-[5px] bg-white py-1 px-3">
+    <div className="w-full flex items-center rounded-[5px] bg-white py-1 px-3">
       <input
         type="text"
         className="outline-none bg-transparent text-grey w-full"
--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -8,13 +8,13 @@ import SendResume from "../components/SendResume";
 
 const Footer: FC = () => {
   return (
-    <footer className="flex flex-col justify-between offset pt-[5%] pb-5 h-[270px] bg-bleuDark">
-      <div className="w-full flex justify-between m-auto">
-        <div className="w-1/4">
+    <footer className="flex flex-col justify-between py-[5%] !pb-0 bg-bleuDark">
+      <div className="w-full offset flex justify-between m-auto sm:flex-col">
+        <div className="w-1/4 sm:w-full sm:py-10">
           <p className="font-semibold text-blue">Reach me on</p>
           <SocialNetwork />
         </div>
-        <div className="w-1/4">
+        <div className="w-1/4 sm:w-full sm:py-10">
           <p className="font-semibold text-blue">Usefull link</p>
           <div className="flex flex-col mt-[20px]">
             <Link to="/">Home</Link>
@@ -23,14 +23,14 @@ const Footer: FC = () => {
             <Link to="/contact">Contact</Link>
           </div>
         </div>
-        <div className="w-1/4">
+        <div className="w-1/4 sm:w-full sm:py-10">
           <p className="font-semibold text-blue">Receive my resume</p>
           <div className="flex items-center justify-between mt-[20px]">
             <SendResume />
           </div>
         </div>
       </div>
-      <p className="text-[12px] text-center text-grey">
+      <p className="text-[12px] text-center text-grey p-10">
         Copyright Tojonirina 2023
       </p>
     </footer>
